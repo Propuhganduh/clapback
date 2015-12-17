b44e74cdd7deea8adea5eccec78bf263584c15a0
Propuhganduh <JosephLG27@outlook.com>
2015-12-16 21:44:20 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,8 +5,8 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/xcool guy/;  botRegexDL = /^\/DDL/i;botRegexSalt = /^\/salt/;botRegexYub = /^\/yubnub/
-      botRegexAd=/^\/advance/;botRegexBan = /^\/ban/; botRegexSC = /^\/SDL/i;
+      botRegex = /^\/xcool guy/;  botRegexRip = /^\/rip/; botRegexStop = /^\/stop/;
+      botRegexProp = /^\/prop/;botRegexKys = /^\/kys/; botRegexSC = /^\/SDL/i;
       botRegexP = /^\/PDL/i;  botRegexTw = /^\/twitch/i; botRegexSb = /^\/sub/; botRegexCh = /^\/cheese/;
       botRegexCMN = /^\/Chad McKnight nudes/; botRegexh2h = /^\/h2h/; botRegexDef = /^\/defense/
   var teamAb = ["NE","NO","ARI","PHI","CLE","TEN","OAK","DAL","IND","SEA","CIN","PIT","JAC"
@@ -17,72 +17,24 @@ function respond() {
     postMessage(cool());
     this.res.end();
   } 
-  else if(request.text && botRegexDL.test(request.text)) {
+  else if(request.text && botRegexRip.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://daddyleagues.com/F1TG/team/"+request.text.substring(5,8)+"/depthchart");
+    postMessage("https://i.groupme.com/600x610.png.ce11c9923a3741c0b837f19d31e87b2e");
     this.res.end();
   } 
-  else if(request.text && botRegexSalt.test(request.text)) {
+  else if(request.text && botRegexStop.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://i.imgur.com/q3ohLtD.gif");
+    postMessage("Stop appropriating black culture");
     this.res.end();
   } 
-  else if(request.text && botRegexAd.test(request.text)) {
+  else if(request.text && botRegexProp.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://www.kno.com/images/store/knoadvance/overview.png");
+    postMessage("https://i.groupme.com/409x327.jpeg.08b5d95c51bb4897835cfe6b514f2f52.large");
     this.res.end();
   }
-  else if(request.text && botRegexYub.test(request.text)) {
+  else if(request.text && botRegexKys.test(request.text)) {
     this.res.writeHead(200);
-    postMessage("http://www.quickmeme.com/img/9f/9f3720469d1ce6c2d20130ed0750935a394df80ffcddec6d16e091d95efea854.jpg");
-    this.res.end();
-  } 
-  else if(request.text && botRegexBan.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("http://www.returnofkings.com/wp-content/uploads/2014/03/ban.jpg");
-    this.res.end();
-  } 
-  else if(request.text && botRegexSC.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("http://daddyleagues.com/F1TG/team/"+request.text.substring(5,8)+"/schedule");
-    this.res.end();
-  }
-  else if(request.text && botRegexP.test(request.text)) {
-    this.res.writeHead(200);
-    var req = request.text.substring(5,request.text.length);
-    var rep = req.replace(/ /,"+");
-    postMessage("http://daddyleagues.com/F1TG/players?name="+rep+"&position=all&team=all");
-    this.res.end();
-  } 
-
-  else if(request.text && botRegexTw.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("http://www.twitch.tv/"+request.text.substring(8,request.text.length));
-    this.res.end();
-  } 
-  else if(request.text && botRegexSb.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("http://www.reddit.com/r/fitg");
-    this.res.end();
-  }
-  else if(request.text && botRegexCh.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("http://content.science20.com/files/images/not%20GMO%20vegan%20cheese%20but%20could%20you%20tell%20anyway.jpg");
-    this.res.end();
-  } 
-  else if(request.text && botRegexCMN.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("http://i.groupme.com/236x231.png.9f975e73eab5431094746b0b30f0945e.large");
-    this.res.end();
-  } 
-  else if(request.text && botRegexh2h.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("http://i.groupme.com/1350x904.png.f5047b2f1c154316abd1b2af6ddd3bdf.large");
-    this.res.end();
-  } 
-  else if(request.text && botRegexDef.test(request.text)) {
-    this.res.writeHead(200);
-    postMessage("http://crimsoncast.com/wp-content/uploads/2010/12/D-Fence.jpg");
+    postMessage("http://www.wikihow.com/Tie-a-Noose");
     this.res.end();
   } 
   else {
